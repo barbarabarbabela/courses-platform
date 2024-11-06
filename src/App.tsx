a9826718482a7b8c1b33be4c6a825220ffacf3ca
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 17:14:30 -0300

    feat: render all components on app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Header } from './components/Header'
+import { CourseList } from "./components/CourseList";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="flex">
+      <div className="bg-gray800 flex-1 h-screen">
+        <Header />
+        <Sidebar />
+      </div>
+      <div>
+        <CourseList />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
