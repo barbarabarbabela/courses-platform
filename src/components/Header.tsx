3083d0d0e1a169615ba249650f081de298df0b63
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 17:14:11 -0300

    refactor: change header css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
-    <header className="bg-gray800 p-8 flex justify-center items-center gap-5">
+    <header className="p-2 flex items-center gap-5 mt-5">
       <img width="50" height="50" src={logo} alt="education" />
       <h1 className="text-xl font-bold">Courses Platform</h1>
     </header>
-  )
-}
+  );
+};
