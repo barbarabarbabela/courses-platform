5a3408f5007add2aef89e1364a75b87b00f9a51d
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 21:12:23 -0300

    feat: create inscriptions page

---
+++ src/pages/InscriptionsPage.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+import { InscriptionsList } from "../components/InscriptionsList";
+
+export const InscriptionsPage = () => {
+  return (
+    <div>
+      <Header />
+      <InscriptionsList />
+    </div>
+  );
+};
