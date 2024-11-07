309bca6a9f74116b073ca4a290817f11778ff560
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 21:14:25 -0300

    feat: add menu provider, router and query client to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './global.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MenuProvider } from "./contexts/menu-context.tsx";
+import { InscriptionsPage } from "./pages/InscriptionsPage.tsx";
+import App from "./pages/App.tsx";
+import { CourseDetailsPage } from "./pages/CourseDetailsPage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/inscricoes",
+    element: <InscriptionsPage />,
+  },
+  {
+    path: "/details",
+    element: <CourseDetailsPage />,
+  },
+]);
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <MenuProvider>
+        <RouterProvider router={router} />
+      </MenuProvider>
+    </QueryClientProvider>
   </StrictMode>
-)
+);
