329b9345f585cffe584f9eb555434a9361d42c94
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 21:12:02 -0300

    refactor: change hook ans context name to menu

---
+++ src/contexts/menu-context.tsx
@@ -0,0 +1,38 @@
+import { createContext, useState, ReactNode } from "react";
+import { PiBooks, PiStar } from "react-icons/pi";
+
+type MenuItem = {
+  label: "Todos os cursos" | "Inscrições";
+  icon: JSX.Element;
+  path: string;
+};
+
+type MenuContextType = {
+  selected: MenuItem["label"];
+  items: MenuItem[];
+  handleSelect: (item: MenuItem["label"]) => void;
+};
+
+const MenuContext = createContext<MenuContextType | undefined>(undefined);
+
+const MenuProvider = ({ children }: { children: ReactNode }) => {
+  const [selected, setSelected] =
+    useState<MenuItem["label"]>("Todos os cursos");
+
+  const items: MenuItem[] = [
+    { label: "Todos os cursos", icon: <PiBooks />, path: "/" },
+    { label: "Inscrições", icon: <PiStar />, path: "/inscricoes" },
+  ];
+
+  const handleSelect = (item: MenuItem["label"]) => {
+    setSelected(item);
+  };
+
+  return (
+    <MenuContext.Provider value={{ selected, items, handleSelect }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+export { MenuProvider, MenuContext };
