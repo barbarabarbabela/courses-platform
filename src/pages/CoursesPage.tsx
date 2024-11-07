edefcd43281616dadee5e05ecd74b832ad23d10a
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 21:12:14 -0300

    feat: create courses page

---
+++ src/pages/CoursesPage.tsx
@@ -0,0 +1,11 @@
+import { CourseList } from "../components/CourseList";
+import { Header } from "../components/Header";
+
+export const CoursesPage = () => {
+  return (
+    <div>
+      <Header />
+      <CourseList />
+    </div>
+  );
+};
