bae13b815d4fc4213062a89de425ea90c4f88467
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 22:23:09 -0300

    feat: add courses query infos to course list component

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,14 +1,24 @@
+import { useCoursesQuery } from "../hooks/use-courses-query";
 import { CourseCard } from "./ui/CourseCard";
 
 export const CourseList = () => {
+  const { data } = useCoursesQuery();
+
   return (
     <div className="p-10">
       <h2 className="text-3xl font-bold mb-10">Todos os Cursos</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {data?.map((course) => {
+          return (
+            <CourseCard
+              title={course.title}
+              instructor={course.instructor}
+              description={course.description}
+              img={course.image}
+              key={course.id}
+            />
+          );
+        })}
       </div>
     </div>
   );
