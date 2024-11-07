0a25d6c3f5e3994063c8524391b7d6b4879a327a
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-11-06 21:13:35 -0300

    feat: create inscriptions list component

---
+++ src/components/InscriptionsList.tsx
@@ -0,0 +1,8 @@
+export const InscriptionsList = () => {
+  return (
+    <div className="p-10">
+      <h2 className="text-3xl font-bold mb-10">Inscrições</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"></div>
+    </div>
+  );
+};
