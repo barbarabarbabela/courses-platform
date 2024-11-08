19f0152667dae0f999d070606421b1ba7e1b5482
Barbara <barbarabarbabela@gmail.com>
2024-11-08 15:40:12 -0300

    feat: add search funcionality on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,57 @@
 import { FaSearch } from "react-icons/fa";
 import logo from "../assets/logo.png";
-import Input from "./ui/Input";
+import Input from "./Input";
 import { useMenu } from "../hooks/use-menu";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useSearchContext } from "../hooks/use-search-context";
+
+interface FormProps {
+  inputData: string;
+}
 
 export const Header = () => {
   const { selected, items, handleSelect } = useMenu();
+  const { setSearchTerm } = useSearchContext();
+  const { handleSubmit, register, reset } = useForm<FormProps>();
+
+  const submitForm = (data: FormProps) => {
+    setSearchTerm(data.inputData.toLowerCase());
+    reset();
+  };
 
   return (
     <header className="p-4 bg-gray800 flex flex-col md:flex-row items-center w-full justify-around">
       <div className="flex items-center gap-3 md:gap-5">
         <img width="50" height="50" src={logo} alt="education" />
-        <h1 className="text-xl font-bold">Courses Platform</h1>
+        <h1 className="text-2xl font-bold">Courses Platform</h1>
       </div>
 
       <div className="flex text-lg gap-5 md:gap-10 mt-2 md:mt-0">
         {items.map((item) => (
-          <span
-            key={item.label}
-            onClick={() => handleSelect(item.label)}
-            className={`cursor-pointer hover:text-green500 ${
-              selected === item.label ? "border-b-4 border-green500" : ""
-            }`}
-          >
-            <Link to={item.path}>{item.label}</Link>
-          </span>
+          <Link key={item.label} to={item.path}>
+            <span
+              onClick={() => handleSelect(item.label)}
+              className={`cursor-pointer hover:text-green500 ${
+                selected === item.label ? "border-b-4 border-green500" : ""
+              }`}
+            >
+              {item.label}
+            </span>
+          </Link>
         ))}
       </div>
 
       <div className="w-full md:w-auto mt-5 md:mt-0 flex justify-center ">
-        <Input placeholder="O que você está buscando?" icon={<FaSearch />} />
+        <form onSubmit={handleSubmit(submitForm)} className="flex gap-3">
+          <Input
+            placeholder="O que você está buscando?"
+            {...register("inputData")}
+          />
+          <button type="submit">
+            <FaSearch size={20} />
+          </button>
+        </form>
       </div>
     </header>
   );
