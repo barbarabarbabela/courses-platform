9fb1047d1e452e56a1752ffc2b9b12b39523085b
Barbara <barbarabarbabela@gmail.com>
2024-11-08 15:39:54 -0300

    feat: add search and inscription provider on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,42 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./global.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { MenuProvider } from "./contexts/menu-context.tsx";
-import { InscriptionsPage } from "./pages/InscriptionsPage.tsx";
-import App from "./pages/App.tsx";
-import { CourseDetailsPage } from "./pages/CourseDetailsPage.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { MenuProvider } from './contexts/menu-context.tsx'
+import { InscriptionsPage } from './pages/InscriptionsPage.tsx'
+import App from './pages/App.tsx'
+import { CourseDetailsPage } from './pages/CourseDetailsPage.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SearchProvider } from './contexts/search-context.tsx'
+import { InscriptionProvider } from './contexts/inscription-context.tsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/inscricoes",
+    path: '/inscricoes',
     element: <InscriptionsPage />,
   },
   {
-    path: "/details",
+    path: '/detalhes/:id',
     element: <CourseDetailsPage />,
   },
-]);
+])
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MenuProvider>
-        <RouterProvider router={router} />
+        <SearchProvider>
+          <InscriptionProvider>
+            <RouterProvider router={router} />
+          </InscriptionProvider>
+        </SearchProvider>
       </MenuProvider>
     </QueryClientProvider>
   </StrictMode>
-);
+)
