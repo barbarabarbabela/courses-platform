be47eb71ffb26e6dfaa8c44b5d48b7c2306faf49
Barbara <barbarabarbabela@gmail.com>
2024-11-08 15:35:54 -0300

    feat: create course details page

--- src/pages/CourseDetailsPage.tsx
+++ src/pages/CourseDetailsPage.tsx
@@ -1,8 +1,9 @@
+import { CourseDetailsCard } from '../components/CourseDetailsCard'
+
 export const CourseDetailsPage = () => {
   return (
-    <div className="p-10">
-      <h2 className="text-3xl font-bold mb-10">Nome do curso</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"></div>
+    <div>
+      <CourseDetailsCard />
     </div>
-  );
-};
+  )
+}
