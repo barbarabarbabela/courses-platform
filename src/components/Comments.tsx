a7231e4cd95330376eda5ab10cc811048f731469
Barbara <barbarabarbabela@gmail.com>
2024-11-09 18:33:35 -0300

    feat: create comments component

---
+++ src/components/Comments.tsx
@@ -0,0 +1,39 @@
+import { Review } from "../interfaces/review";
+import StarRating from "./Rating";
+
+interface CommentsProps {
+  reviews: Review[];
+}
+
+export const Comments = ({ reviews }: CommentsProps) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold mb-5">
+        Avaliações ({reviews.length})
+      </h2>
+      <ul className="flex flex-col gap-5">
+        {reviews.map((review) => (
+          <li
+            key={review.comment}
+            className="border border-gray-600 bg-gray700 p-5 rounded-lg"
+          >
+            <div className="flex gap-2 items-center">
+              <img
+                src={review.image}
+                className="w-20 h-20 rounded-full object-cover shadow-md"
+                alt={`Foto de ${review.user}`}
+              />
+              <div className="flex gap-10">
+                <span className="font-bold text-lg">{review.user}</span>
+                <StarRating rating={review.rating} />
+              </div>
+            </div>
+            <div className="mt-5 text-xl">
+              <p className="mb-1">"{review.comment}"</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
